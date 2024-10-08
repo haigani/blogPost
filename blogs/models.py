ab0456325f6df46ff92601ef6828c7924d5fb472
Hanieh Aigani <haniye_aygani@yahoo.com>
2024-10-07 22:42:38 -0400

    the models created.

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comments(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    text = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # How top return comments related to a post
+    # def __str__(self):
+    #     return
